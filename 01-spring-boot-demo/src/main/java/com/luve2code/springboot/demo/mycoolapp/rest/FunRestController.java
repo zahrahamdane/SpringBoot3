2a9d9e65abc04afacf845a8990da2952dd9625f5
Zahra Goarant <zahra.goarant@externe.systeme-u.fr>
2024-09-26 00:17:38 +0200

    spring boot

---
+++ 01-spring-boot-demo/src/main/java/com/luve2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.luve2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" than return "Hello World !"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+}
